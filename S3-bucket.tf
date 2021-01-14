5eb31778ba3c693b25e43830534b8f65dcad1147
Scott Conkey <conkeya@battelle.org>
2021-01-14 21:26:27 +0000

    Added s3 bucket module

--- S3-bucket.tf
+++ S3-bucket.tf
@@ -2,11 +2,11 @@ module "s3-bucket" {
   source  = "app.terraform.io/ScottConkey-Training/s3-bucket/aws"
   version = "1.17.0"
   # insert required variables here
-acceleration_status = ""
-bucket = ""
+acceleration_status 
+bucket 
 bucket_prefix = "scottconkey"
-policy = ""
-request_payer = ""
+policy 
+request_payer
 
 credentials "app.terraform.io" {
   # valid user API token:
