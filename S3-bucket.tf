5344a6c159216a0038f3116116ad5bb3b10c25c8
Scott Conkey <conkeya@battelle.org>
2021-01-14 21:23:25 +0000

    Added s3 bucket module

--- S3-bucket.tf
+++ S3-bucket.tf
@@ -7,4 +7,10 @@ bucket = ""
 bucket_prefix = "scottconkey"
 policy = ""
 request_payer = ""
+
+credentials "app.terraform.io" {
+  # valid user API token:
+  token = "bpSNGga8IhS9zw.atlasv1.qu7B8LryN8wxyAfIpjChURZsczpqY7iRPryJyQxIfCtDWabo0Nnm7zmT3kt2QLU2ACI"
+
+
 }
