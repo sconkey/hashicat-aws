ec2eb779561cec71d0111ecb4e9a0acd60103c7d
Scott Conkey <conkeya@battelle.org>
2021-01-14 21:20:04 +0000

    Added s3 bucket module

--- S3-bucket.tf
+++ S3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/ScottConkey-Training/s3-bucket/aws"
-  version = "1.16.0"
+  version = "1.17.0"
   # insert required variables here
 acceleration_status = ""
 bucket = ""
